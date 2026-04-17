7632526f6168a17a8571c1274cbffe570286df68
Rohit Ahari <pro.rohitahari@gmail.com>
2026-04-17 09:06:39 +0000

    Fix: load .env properly with pydantic settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,9 @@
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-
+    ALGORITHM: str = "HS256"
     ENV: str = "dev"
 
-    # 🔐 JWT CONFIG (ADD THIS)
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-
-    class Config:
-        env_file = ".env"
-
-
-settings = Settings()
+    model_config = SettingsConfigDict(env_file=".env")
